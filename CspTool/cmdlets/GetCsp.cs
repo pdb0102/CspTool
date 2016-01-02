938f3338f09c8dd15c390b2d4a3e5c5a38b7902c
AM <am@amaic.de>
2016-01-02 15:45:57 +0100

    Get-Csp Comdlet

---
+++ CspTool/cmdlets/GetCsp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace amaic.de.csptool.cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "Csp")]
+    public class GetCsp : Cmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            WriteObject(Crypt.EnumerateProviderTypes());
+        }
+    }
+}
